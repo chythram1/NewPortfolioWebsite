a8c728ee982c035fb272e459ae78760b1b05757f
chythram1 <chythram@vt.edu>
2026-01-03 14:39:22 -0500

    added colorbend background, experimenting with three js to make the webpage more dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ColorBends from "./components/colorbends";
 import { Navbar } from "./components/navbar";
 import { HeroSection } from "./components/herosection";
 import { AboutSection } from "./components/about";
@@ -11,6 +12,18 @@ import { ScrollReveal } from "./components/scrollreveal";
 function App() {
   return (
     <div>
+       <ColorBends
+        colors={["#ff5c7a", "#8a5cff", "#114554ff"]}
+        rotation={0}
+        speed={0.2}
+        scale={1.0}
+        frequency={1.0}
+        warpStrength={1.0}
+        mouseInfluence={1}
+        parallax={0.5}
+        noise={0.04}
+        transparent={false}
+      />
       <Navbar />
       <HeroSection />
       
